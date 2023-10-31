39f791d32a4b5c719a0470673363db75458fa9a2
Ferrum00 <107812690+Ferrum00@users.noreply.github.com>
2023-10-31 19:37:33 +0500

    Create math.h

---
+++ math.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "locale.h"
+
+float x, y, res;
+char op;
+
+float *pX = &x;
+float *pY = &y;
+float *pRes = &res;
+char *pOp = &op;
+
+float check(char *sym){
+    switch (*sym) {
+        case '+':
+            return *pRes = *pX + *pY;
+            break;
+        case '-':
+            return *pRes = *pX - *pY;
+            break;
+        case '*':
+            return *pRes = *pX * *pY;
+            break;
+        case '/':
+            return *pRes = *pX / *pY;
+            break;
+    }
+    return 0;
+}
